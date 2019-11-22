cc824dd80e9f640b3eac9c9005b07f169fc3a3b6
Tran Minh Nhat <trminhnhat99@gmail.com>
2019-11-22 23:17:19 +0700

    Update new DataBase

--- data gaming center.sql
+++ data gaming center.sql
@@ -3,8 +3,8 @@ use quanlygamingcenter;
 
 -- Tạo các bảng
 create table acc(
-    id int auto_increment, -- id tài khoản
-    acc_name varchar(18) unique not null, -- tên tài khoản
+	id int auto_increment, -- id tài khoản
+	acc_name varchar(18) unique not null, -- tên tài khoản
     passwd varchar(18) not null, -- mật khẩu
     date_created date not null, -- ngày tạo tài khoản
     money float default(0) not null, -- số dư trong tài khoản
@@ -12,14 +12,14 @@ create table acc(
 );
 
 create table com_area(
-    id int auto_increment, -- id khu
+	id int auto_increment, -- id khu
     area_name varchar(18) unique not null, -- tên khu
     cost_per_hour float not null, -- giá tiền cho mỗi giờ sử dụng
     primary key (id)
 );
 
 create table computer(
-    id int auto_increment, -- id máy
+	id int auto_increment, -- id máy
     area_id int not null, -- máy thuộc khu nào
     stt enum('mainten', 'in use', 'not in use') not null,
     -- trạng thái: bảo trì / đang sử dụng / còn trống
@@ -30,13 +30,13 @@ create table computer(
 );
 
 create table type_of_goods(
-    id int auto_increment, -- id loại hàng
+	id int auto_increment, -- id loại hàng
     type_name varchar(18) unique not null, -- tên loại hàng
     primary key (id)
 );
 
 create table goods(
-    id int auto_increment, -- id sản phẩm
+	id int auto_increment, -- id sản phẩm
     goods_name varchar(18) unique not null, -- tên sản phẩm
     type_id int, -- sản phẩm thuộc loại nào
     unit varchar(10) not null, -- đơn vị sản phẩm
@@ -47,20 +47,18 @@ create table goods(
 );
 
 create table bill(
-    id int auto_increment, -- id hóa đơn
+	id int auto_increment, -- id hóa đơn
     acc_id int, -- hóa đơn của tài khoản nào
     date_created date not null, -- ngày tạo hóa đơn
-    total float not null, -- tổng tiền hóa đơn
     primary key (id),
     foreign key (acc_id) references acc(id)
 );
 
 create table single_order(
-    id int auto_increment, -- id đơn hàng
+	id int auto_increment, -- id đơn hàng
     goods_id int, -- id sản phẩm đặt mua
     bill_id int, -- id hóa đơn tổng
-    amount float not null, -- số lượng sản phẩm
-    price float not null, -- giá đơn hàng
+    amount int not null, -- số lượng sản phẩm
     primary key (id),
     foreign key (goods_id) references goods(id),
     foreign key (bill_id) references bill(id)
@@ -101,22 +99,10 @@ insert into computer(area_id, stt)
 value(1, 'mainten');
 
 insert into computer(area_id, stt)
-value(1, 'mainten');
-
-insert into computer(area_id, stt)
-value(1, 'not in use');
-
-insert into computer(area_id, stt)
-value(1, 'not in use');
-
-insert into computer(area_id, stt)
 value(1, 'not in use');
 
 insert into computer(area_id, stt, acc_id)
 value(1, 'in use', 4);
-
-insert into computer(area_id, stt, acc_id)
-value(1, 'in use', 1);
 
 insert into computer(area_id, stt, acc_id)
 value(2, 'in use', 2);
@@ -130,41 +116,48 @@ value(4, 'in use', 3);
 insert into computer(area_id, stt)
 value(4, 'not in use');
 
-insert into computer(area_id, stt)
-value(4, 'not in use');
-
-insert into computer(area_id, stt, acc_id)
-value(3, 'in use', 3);
-
 -- Nhập loại mặt hàng
 insert into type_of_goods(type_name)
 value("drink");
+
 insert into type_of_goods(type_name)
 value("food");
+
 insert into type_of_goods(type_name)
 value("card");
 
+-- Nhập các mặt hàng
+insert into goods(goods_name, type_id, unit, price, stock) 
+values('247', 1, 'chai', 10000, 50);
 
--- Chức năng
-select * from com_area;
+insert into goods(goods_name, type_id, unit, price, stock) 
+values('mi tom trung', 2, 'bat', 13000, 20);
 
-select * from type_of_goods;
+insert into goods(goods_name, type_id, unit, price, stock) 
+values('garena 50', 3, 'chiec', 50000, 30);
 
-select id, 
-    (select area_name from com_area 
-        where com_area.id = computer.area_id) as 'area', 
-    stt, 
-    (select acc_name from acc 
-        where acc.id = computer.acc_id) as 'account' 
-from computer;
+-- Nhập các hóa đơn tổng
+insert into bill(acc_id, date_created)
+value(1, current_date());
 
-select * from acc;
+insert into bill(acc_id, date_created)
+value(2, current_date());
 
-select id, goods_name, 
-    (select type_name from type_of_goods 
-        where type_of_goods.id = goods.type_id) as 'type', 
-    unit, 
-    price, 
-    stock 
-from goods;
+insert into bill(acc_id, date_created)
+value(3, current_date());
 
+insert into bill(acc_id, date_created)
+value(4, current_date());
+
+-- Nhập các đơn hàng
+insert into single_order(goods_id, bill_id, amount) 
+values(1, 1, 1);
+
+insert into single_order(goods_id, bill_id, amount) 
+values(1, 2, 1);
+
+insert into single_order(goods_id, bill_id, amount) 
+values(1, 3, 1);
+
+insert into single_order(goods_id, bill_id, amount) 
+values(1, 4, 1);
